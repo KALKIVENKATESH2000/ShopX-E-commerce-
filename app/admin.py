ad7891add29e611ceb22692c867cf5f542e1e93e
Kalki Venkatesh <kalkivenkateshdonga@gmail..com>
2022-09-27 16:15:32 +0530

    adding

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from . models import Customer, Product, OrderPlaced, Cart
+from django.utils.html import format_html
+from django.urls import reverse
 
 # Register your models here.
 @admin.register(Customer)
@@ -12,8 +14,15 @@ class ProductModelAdmin(admin.ModelAdmin):
 
 @admin.register(OrderPlaced)
 class OrderPlacedModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'customer', 'product', 'quantity', 'ordered_date', 'status']
+    list_display = ['id', 'user', 'customer','customer_info', 'product','product_info', 'quantity', 'ordered_date', 'status']
 
+    def customer_info(self, obj):
+        link = reverse("admin:app_customer_change", args=[obj.customer.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.customer.name)
+    def product_info(self, obj):
+        link = reverse("admin:app_product_change", args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.product.title)
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
     list_display =['id', 'user', 'product', 'quantity']
+
